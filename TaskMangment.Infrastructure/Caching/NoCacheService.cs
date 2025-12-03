8c2bd4f3317ae8acb4cd5f44281f5a06f6a88a4e
aahmed <aahmed@uplandsoftware.com>
2025-12-03 14:32:11 +0200

    fix cash

---
+++ TaskMangment.Infrastructure/Caching/NoCacheService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMangment.Application.Common.Interfaces;
+
+namespace TaskMangment.Infrastructure.Caching
+{
+    public class NoCacheService : ICachingService
+    {
+        public Task<T> GetAsync<T>(string key)
+        {
+            return Task.FromResult(default(T));
+        }
+
+        public Task SetAsync<T>(string key, T value, TimeSpan expiration)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
