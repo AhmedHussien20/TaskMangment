b549ab5037f3c2d6d129217a543913f516f96cce
aahmed <aahmed@uplandsoftware.com>
2025-12-25 16:00:38 +0200

    add the attachment

--- TaskMangment.Application/DTOs/EmployeeDTO.cs
+++ TaskMangment.Application/DTOs/EmployeeDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -39,7 +40,10 @@ namespace TaskMangment.Application.DTOs
         public string Email { get; set; }
 
         [MaxLength(500)]
-        public string Password { get; set; } 
+        public string Password { get; set; }
+
+        public List<IFormFile>? Attachments { get; set; }
+
     }
 
     public class EmployeeGetDto
@@ -48,6 +52,7 @@ namespace TaskMangment.Application.DTOs
         public string FullName { get; set; }
         public string? Title { get; set; }
         public string BranchName { get; set; }
+        public string BranchId { get; set; }
         public string Email { get; set; }
         public string Mobile { get; set; }
         public string Qualification { get; set; }
