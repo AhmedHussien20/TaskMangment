09a08154e93346a45fb6e60b1e3a042938d81968
aahmed <aahmed@uplandsoftware.com>
2025-12-26 16:26:22 +0200

    fix font

--- TaskMangment.Presentation/TaskMangmentFE/src/app/app.component.ts
+++ TaskMangment.Presentation/TaskMangmentFE/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import * as NavActions from './store/nav/nav.actions';
 import { TranslationService } from './shared/services/translation.service';
 import { SignalRService } from './core/services/signalr.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +17,17 @@ import { SignalRService } from './core/services/signalr.service';
 })
 export class AppComponent {
   title = 'Task-Portal';
-  constructor(private appState: AppStateService, private store: Store, private translationService: TranslationService, private signalR: SignalRService) { }
+  constructor(private appState: AppStateService, private store: Store, private translationService: TranslationService, private signalR: SignalRService,private translate: TranslateService) { }
 
   ngOnInit(): void {
     const userId = 4
     this.store.dispatch(NavActions.initializeMenu());
     this.appState.updateState();
- 
+    const savedLang = localStorage.getItem('lang') || 'ar';
+
+    this.translate.use(savedLang);
+
+    document.documentElement.lang = savedLang;
+    document.documentElement.dir = savedLang === 'ar' ? 'rtl' : 'ltr';
   }
 }
