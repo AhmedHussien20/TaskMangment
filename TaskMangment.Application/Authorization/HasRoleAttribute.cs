929126342e451b7fd38d295364d7c363f88edc44
aahmed <aahmed@uplandsoftware.com>
2025-12-26 17:30:57 +0200

    add roles Attribute

---
+++ TaskMangment.Application/Authorization/HasRoleAttribute.cs
@@ -0,0 +1,16 @@
+﻿ 
+
+namespace TaskMangment.Application.Authorization
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class HasRoleAttribute : Attribute
+    {
+        public string [] Roles { get; }
+
+        public HasRoleAttribute(params string[] roles)
+        {
+            Roles = roles;
+        }
+    }
+}
+
