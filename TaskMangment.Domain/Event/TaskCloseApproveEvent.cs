a9d93c5f207fb787cedd00c9e1cfdd9092aa4b77
Rawan Ragab <rawanragab758@gmail.com>
2026-01-01 12:41:48 +0200

    commit my work

---
+++ TaskMangment.Domain/Event/TaskCloseApproveEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMangment.Domain.Event
+{
+    public class TaskCloseApproveEvent
+    {
+        public int TaskId { get; }
+        public string TaskTitle { get; }
+        public List<int> AssignedEmployeeIds { get; }
+
+        public TaskCloseApproveEvent(int taskId, string taskTitle, List<int> assignedEmployeeIds)
+        {
+            TaskId = taskId;
+            TaskTitle = taskTitle;
+            AssignedEmployeeIds = assignedEmployeeIds;
+        }
+    }
+}
---
+++ TaskMangment.Domain/Event/TaskExtendApproveEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMangment.Domain.Event
+{
+    public class TaskExtendApproveEvent
+    {
+        public int TaskId { get; }
+        public string TaskTitle { get; }
+
+        public DateTime? OldDueDate { get; }
+        public DateTime? NewDueDate { get; }
+        public List<int> AssignedEmployeeIds { get; }
+
+        public TaskExtendApproveEvent(int taskId, string taskTitle, DateTime? oldDueDate, DateTime? newDueDate, List<int> assignedEmployeeIds)
+        {
+            TaskId = taskId;
+            TaskTitle = taskTitle;
+            NewDueDate = newDueDate;
+            OldDueDate = oldDueDate;
+            AssignedEmployeeIds = assignedEmployeeIds;
+        }
+    }
+}
